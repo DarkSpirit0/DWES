701654d30fa15e49c4e37e6d9eef36d4d37ad28c
Antonio Sillero <sillerortizantonio@gmail.com>
2025-03-24 09:52:32 +0100

    Blade recuperacion

---
+++ Recuperacion/Laravel_12/Blade/Vistas/resources/views/_components/card.blade.php
@@ -0,0 +1,5 @@
+<div style="border: 1px solid #000000; margin: 10px; padding: 10px;">
+    <h3>{{$title}}</h3>
+    <img src="{{ asset('assets/img/descarga.jpg') }}" alt="mi foto" width="128">
+    <p>{{$content}}</ </p>
+</div>
---
+++ Recuperacion/Laravel_12/Blade/Vistas/resources/views/layouts/_partials/menu.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <ul>
+            <ul>
+                <li><a href="/">index</a></li>
+                <li><a href="/services">services</a></li>
+                <li><a href="/about">about</a></li>
+                <li><a href="/contact">contact</a></li>
+            </ul>
+        </ul>
+    </nav>
+
+</header>
