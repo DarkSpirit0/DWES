93a661130ca8a991f75d442ad48012544b8b12f4
Antonio Sillero <sillerortizantonio@gmail.com>
2025-04-06 16:53:05 +0200

    Controladores 2

--- Recuperacion/Laravel_12/4_Controladores/controllers/routes/web.php
+++ Recuperacion/Laravel_12/4_Controladores/controllers/routes/web.php
@@ -4,5 +4,5 @@ use App\Models\Player;
 use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PlayerController::class, 'index'])->name('user.index');
-Route::get('/create', [PlayerController::class, 'create'])->name('user.create');
+Route::get('/', [PlayerController::class, 'index'])->name('player.index');
+Route::get('/create', [PlayerController::class, 'create'])->name('player.create');
